92d15fb8a67fcdfdf7728ca28b52d49615b5e637
Sheldonhh <xphuangsg@gmail.com>
2022-12-26 15:21:36 +0800

    update links

--- generate_link.sh
+++ generate_link.sh
@@ -1,1 +1,1 @@
-for i in $(seq 1 82); echo "https://github.com/SheldonHH/code_visualization/blob/main/Dungen_games/Dungen_games%${i}.jpeg?raw=true" ; done
+for i in $(seq 1 82); do echo "https://github.com/SheldonHH/code_visualization/blob/main/Ones%20and%20Zeros/Ones%21and%20Zeros%20$i.jpeg?raw=true" ; done
